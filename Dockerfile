4c8c5e7508580a39a4d11ab083c3d29a1ebd4cc4
Lucian Ursu <lucian.ursu@gmail.com>
2020-04-02 12:31:59 +0300

    Make users.csv accessible through /etc/tiddlywiki

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM node:11.15.0-alpine
 RUN npm install -g tiddlywiki
 
 VOLUME /var/lib/tiddlywiki/
+VOLUME /etc/tiddlywiki/
 
 WORKDIR /var/lib/tiddlywiki/
 
@@ -10,4 +11,4 @@ EXPOSE 8080
 
 ENTRYPOINT ["tiddlywiki"]
 
-CMD ["--listen", "host=0.0.0.0", "credentials=users.csv", "readers=(authenticated)", "writers=(authenticated)"]
+CMD ["--listen", "host=0.0.0.0", "credentials=/etc/tiddlywiki/users.csv", "readers=(authenticated)", "writers=(authenticated)"]
--- build.sh
+++ build.sh
@@ -14,7 +14,13 @@ docker build . -t tiddlywiki.fat:$1
 # Shrink the image with docker-slim
 # The `--cmd` makes sure we can run that command on the target server
 # `--mount` keeps the reference to the volume in the image, so that we can use it when deployed
-docker-slim --report 'off' build --cmd '["--init", "server"]' --mount "$(pwd)/wiki":/var/lib/tiddlywiki --target tiddlywiki.fat:$1 --tag elbear/tiddlywiki:$1
+docker-slim --report 'off' \
+            build \
+            --cmd '["--init", "server"]' \
+            --mount "$(pwd)/wiki":/var/lib/tiddlywiki \
+            --mount "$(pwd)/wiki":/etc/tiddlywiki \
+            --target tiddlywiki.fat:$1 \
+            --tag elbear/tiddlywiki:$1
 
 # Remove the wiki directory created in the previous step
 [ -d ./wiki ] && rm -r ./wiki
